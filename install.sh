18e3757fe25056dccbe0c15c00ffe469736950fc
Felix Bernhardt <felix.bernhardt@mailbox.org>
2026-08-10 10:17:40 +0200

    link herdr config

--- install.sh
+++ install.sh
@@ -2,30 +2,23 @@
 # Create softlinks to config files/dirs in the home dir
 #   -s: create softlinks
 #   -n: handle dirs like files, i.e. do not create a link with the dir when it already exists
-#
 
-# First get the absolute dirname of the install script
+# First get the absolute dirname of the dotfiles repository
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Link custom scripts
-ln -sn "${DOTFILES_DIR}/scripts" ~/.local/scripts
-
-# Link zsh files
+# root config files
 ln -sn "${DOTFILES_DIR}/zsh/.zshenv" ~/.zshenv
 ln -sn "${DOTFILES_DIR}/zsh/.zshrc" ~/.zshrc
-
-# Link git config
 ln -sn "${DOTFILES_DIR}/.gitconfig" ~/.gitconfig
-
-# Link tmux config
 ln -sn "${DOTFILES_DIR}/tmux/.tmux.conf" ~/.tmux.conf
 
-# Create .config directory if it doesn't exist
+# files in .local
+mkdir -p ~/.local
+ln -sn "${DOTFILES_DIR}/scripts" ~/.local/scripts
+
+# files in .config
 mkdir -p ~/.config
-
-# Link nvim config
+ln -sn "${DOTFILES_DIR}/herdr/config.toml" ~/.config/herdr/config.toml
 ln -sn "${DOTFILES_DIR}/nvim/" ~/.config/nvim
-
-# Link alacritty config files
 ln -sn "${DOTFILES_DIR}/alacritty.toml" ~/.config/alacritty.toml
 
